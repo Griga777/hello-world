f965c94ece1d27774ffef52b35bc714b5e4c675c
Grigori Uleev <uleev777@yandex.ru>
2020-04-07 17:53:17 +0400

    created first program

---
+++ src/Epam/Main.java
@@ -0,0 +1,11 @@
+package Epam;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
